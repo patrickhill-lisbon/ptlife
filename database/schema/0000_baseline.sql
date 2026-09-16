cc9955e57b03618e49ef2555a8240f5910b72be4
patrickhill-lisbon <patrick.j.hill@gmail.com>
2026-09-16 23:20:25 +0100

    Create 0000_baseline.sql
    Create database baseline schema

---
+++ database/schema/0000_baseline.sql
@@ -0,0 +1,13 @@
+-- PTLife Database
+-- Baseline Schema
+--
+-- This file defines the complete database structure required to
+-- create a new PTLife database from scratch.
+--
+-- It represents the production schema as of 2026-09-16.
+-- Reference/lookup data is maintained separately under database/seed/.
+--
+-- Do not use this file to upgrade an existing production database.
+-- Changes after this baseline belong in database/migrations/.
+
+PRAGMA foreign_keys = ON;
